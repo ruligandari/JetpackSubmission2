2e4a9892d5a922817642ebe382ad02b572600708
ruligandari <ruligandari@gmail.com>
2021-06-04 13:51:48 +0700

    add DetailMoviesViewModel.kt

---
+++ app/src/main/java/com/ruligandari/jetpacksubmission2/ui/detail/DetailMoviesViewModel.kt
@@ -0,0 +1,10 @@
+package com.ruligandari.jetpacksubmission2.ui.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.ruligandari.jetpacksubmission2.data.DataRepository
+import com.ruligandari.jetpacksubmission2.data.source.local.MoviesEntity
+
+class DetailMoviesViewModel(private var mRepository: DataRepository): ViewModel() {
+    fun getListDetailMovies(moviesId: Int): LiveData<MoviesEntity> = mRepository.getMoviesDetail(moviesId)
+}
